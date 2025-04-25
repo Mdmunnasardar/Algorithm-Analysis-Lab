68e893a96e942f098fb257f80400ba1f9008e952
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-04-26 00:38:31 +0600

    iteration and recursion all done

--- Lab Practices/iterationFib.cpp
+++ Lab Practices/iterationFib.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    int fib(int n){
-        int i=1,j=0;
-        for(int k=1;k<=n;++k){
-            j=i+j;
-            i=j-i;
-        }
-        return j;
+int fib(int n)
+{
+    int i = 1, j = 0;
+    for (int k = 1; k <= n; ++k)
+    {
+        j = i + j;
+        i = j - i;
     }
+    return j;
+}
 
-    int main(){
-        int n;
+int main()
+{
+    int n;
 
-        cout<<"Enter the number: "<<endl;
-        cin>>n;
+    cout << "Enter the number: " << endl;
+    cin >> n;
 
-        cout<<"The Sequnce of the Fibonacci : ";
+    cout << "The  Fibonacci number is of " <<n<<"th position : " << fib(n)<<endl;
 
-        for(int i=0;i<=n;++i){
-            cout<<fib(i)<<" ";
-        }
-        return 0;
+    cout << "The Sequnce of the Fibonacci : ";
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << fib(i) << " ";
     }
+    return 0;
+}
