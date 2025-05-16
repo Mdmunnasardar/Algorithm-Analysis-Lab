d97eeeca2c0dbada966a77421f880f31fac187ec
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-05-17 01:41:56 +0600

    Binary search algorithm

---
+++ All in one/binaraysearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Iterative Binary Search Function
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1; // Not found
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found in array" << endl;
+
+    return 0;
+}
